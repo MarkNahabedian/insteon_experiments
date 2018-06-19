c49081c60851e93bbaee689e27cbcc0d1939cf1a
Mark Nahabedian <naha@mit.edu>
2018-06-19 18:42:06 -0400

    Working web server framework.  Doesn't do anything yet.

--- webserver.py
+++ webserver.py
@@ -5,33 +5,38 @@ import http.server
 
 assert sys.version_info >= (3, 0)
 
-class ResponseHandler(http.server.BaseHTTPRequestHandler):
-  def __init__(self):
-    pass
+class MyRequestHandler(http.server.BaseHTTPRequestHandler):
+  def do_HEAD(self):
+    _logger.info("HEAD")
+    self.send_response(200, "Ok")
+    self.send_header('Content-type','text/html')
+    self.end_headers()
+    self.flush_headers()
+
   def do_GET(self):
     _logger.info("GET")
     self.send_response(200, "Ok")
     self.send_header('Content-type','text/html')
     self.end_headers()
-    self.wfile.write("<html><head><title>foo</title></head><body>Hello.</body></html>")
+    self.flush_headers()
+    self.wfile.write(bytes("<html><head><title>foo</title></head><body>Hello.</body></html>", "utf8"))
+    self.wfile.flush()
 
 
-def run(server_class=http.server.HTTPServer,
-        handler_class=ResponseHandler):
+def run():
   server_address = ('', 8000)
-  httpd = server_class(server_address, handler_class)
-  httpd.serve_forever()
+  httpd = http.server.HTTPServer(server_address, MyRequestHandler)
+  try: 
+    _logger.info("Starting Webserver.")
+    httpd.serve_forever()
+  finally:
+    _logger.info("Webserver Exited.")
 
 
 if __name__ == '__main__':
   global _logger
   logging.basicConfig(filename='webserver.log', level=logging.INFO)
   _logger = logging.getLogger(__name__)
-  _logger.info("Starting Webserver.")
-  try:
-    run()
-  finally:
-    _logger.info("Webserver Exited.")
+  run()
   logging.shutdown()
-
 
